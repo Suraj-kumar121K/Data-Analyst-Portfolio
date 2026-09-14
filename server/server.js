1c7d7e6d22f0a864a5d442d3814fa66f09c75cdd
Suraj Kumar <surajkumar09464@gmail.com>
2026-09-15 04:02:04 +0530

    add

--- server/server.js
+++ server/server.js
@@ -12,8 +12,44 @@ app.use(cors());
 // Serve static files from root portfolio folder
 app.use(express.static(path.join(__dirname, "..", "portfolio")));
 
+// Main
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "portfolio", "index.html"));
+});
+
+// Home Page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Home_page", "about.html"));
+});
+
+// Skills
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Skills", "skills.html"));
+});
+
+// "Projects"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Projects", "projects.html"));
+});
+
+// Experience
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Experience", "experience.html"));
+});
+
+// Education
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Education", "education.html"));
+});
+
+// Resume
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Resume", "resume.html"));
+});
+
+// Contact
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "portfolio", "Contact", "contact.html"));
 });
 
 app.listen(PORT, () => {
